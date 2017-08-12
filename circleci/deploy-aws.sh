8b49a0f47a190b77c4cc1408da662365b3a3c830
Assen Kolov <assen.kolov@gmail.com>
2017-08-12 21:33:26 +0200

    _Xms

--- circleci/deploy-aws.sh
+++ circleci/deploy-aws.sh
@@ -4,19 +4,17 @@
 set +x
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
-mkdir ~/bin
 mv kubectl ~/bin
 PATH=$PATH:$(pwd)/bin
 echo $PATH
 mkdir ~/.kube
 echo $KUBECONFIGDATA | base64 --decode --ignore-garbage > ~/.kube/config
-sed s/%VERSION%/$CIRCLE_SHA1/g k8s-deployment.yml > k8s-deployment-latest.yml
-./kubectl apply -f k8s-deployment-latest.yml
 aws configure set default.region eu-central-1
 aws configure set default.output json
 eval $(aws ecr get-login --region eu-central-1)
 docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/$APP_NAME:$CIRCLE_SHA1 .
-docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/$APP_NAME:$CIRCLE_SHA1
-sed 's/%VERSION%/$CIRCLE_SHA1/g; s/%VERSION%/$CIRCLE_SHA1/g' k8s-deployment.yml > k8s-deployment-latest.yml
+REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
+docker push $REGISTRY/$APP_NAME:$CIRCLE_SHA1
+sed 's/%VERSION%/$CIRCLE_SHA1/g; s/%APP_NAME%/$APP_NAME/g; s/%REGISTRY%/$REGISTRY/g' k8s-deployment.yml > k8s-deployment-latest.yml
 kubectl apply -f k8s-deployment-latest.yml
 kubectl apply -f k8s-service.yml
