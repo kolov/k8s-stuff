934e01877aea432fb11976500b3acea4bdafd0b6
Assen Kolov <assen.kolov@gmail.com>
2017-08-13 10:19:15 +0200

    _Xms

--- circleci/deploy-aws.sh
+++ circleci/deploy-aws.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-set x
+set -x
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl ~/bin
 PATH=$PATH:$(pwd)/bin
 echo $PATH
 mkdir ~/.kube
-aws ecr get-login --region eu-central-1
-eval $(aws ecr get-login --region eu-central-1)
+aws ecr get-login
+eval $(aws ecr)
 docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/$APP_NAME:$CIRCLE_SHA1 .
 REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
 docker push $REGISTRY/$APP_NAME:$CIRCLE_SHA1
