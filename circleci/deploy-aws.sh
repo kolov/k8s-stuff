5dc22328399d18ac500739d284fc9f1af8aa8dc5
Assen Kolov <assen.kolov@gmail.com>
2017-08-13 12:26:41 +0200

    _Xms

--- circleci/deploy-aws.sh
+++ circleci/deploy-aws.sh
@@ -8,9 +8,8 @@ PATH=$PATH:$(pwd)/bin
 echo $PATH
 mkdir ~/.kube
 echo $KUBECONFIGDATA | base64 --decode --ignore-garbage > ~/.kube/config
-aws ecr get-login
 eval $(aws ecr get-login)
-docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/$APP_NAME:$CIRCLE_SHA1 .
+docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/$APP_NAME:$CIRCLE_SHA1 $BUILD_TARGET_DIR
 REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
 docker push $REGISTRY/$APP_NAME:$CIRCLE_SHA1
 sed -e s/%VERSION%/$CIRCLE_SHA1/g -e s/%APP_NAME%/$APP_NAME/g -e s/%REGISTRY%/$REGISTRY/g k8s-deployment.yml > k8s-deployment-latest.yml
