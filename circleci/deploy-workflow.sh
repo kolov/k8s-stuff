70645b8f7f63d87bf447f71425ad12ab1497f816
Assen Kolov <assen.kolov@gmail.com>
2017-10-06 18:55:59 +0200

    --no-include-email

--- circleci/deploy-workflow.sh
+++ circleci/deploy-workflow.sh
@@ -4,7 +4,7 @@ set -x
 mkdir ~/.kube
 $KUBECONFIGDATA | base64 --decode --ignore-garbage > ~/.kube/config
 echo AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID
-eval $(aws ecr get-login)
+eval $(aws ecr get-login --no-include-email)
 docker build --rm=false -t $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/$APP_NAME:$CIRCLE_SHA1 $BUILD_TARGET_DIR
 REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
 docker push $REGISTRY/$APP_NAME:$CIRCLE_SHA1
