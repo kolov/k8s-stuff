246d7959a3fb6149487fc5e8a5dfc368ea5749bf
Assen Kolov <assen.kolov@gmail.com>
2017-10-06 21:26:31 +0200

    --no-include-email

--- circleci/deploy-workflow.sh
+++ circleci/deploy-workflow.sh
@@ -10,4 +10,4 @@ REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
 docker push $REGISTRY/$APP_NAME:$CIRCLE_SHA1
 sed -e s/%VERSION%/$CIRCLE_SHA1/g -e s/%APP_NAME%/$APP_NAME/g -e s/%REGISTRY%/$REGISTRY/g $K8S_DEPLOYMENT > k8s-deployment-latest.yml
 kubectl apply -f k8s-deployment-latest.yml
-kubectl apply -f K8S_SERVICE
+kubectl apply -f $K8S_SERVICE
